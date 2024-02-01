0a12b5081d0f79c2cf0d7b3fe72af8620c1c5733
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-02-01 20:17:35 +0530

    added event sync

--- comments/index.js
+++ comments/index.js
@@ -37,8 +37,30 @@ app.post("/posts/:id/comments", async (req, res) => {
 
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Received Event", req.body.type);
+
+  const { type, data } = req.body;
+  if(type === "CommentModerated"){
+    const {postId, id, status, content} = data;
+    const comments = commentsByPostsId[postId];
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+          id,
+          content,
+          status,
+          postId,
+      }
+  }).catch((err) => {
+      console.log(err.message);
+  });
+  }
+
   res.send({});
 });
 
