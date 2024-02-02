3d96f50244e27f5b1e799c3e7d3a033f5850e842
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-02-02 21:15:25 +0530

    added deployment in kubectl

--- posts/index.js
+++ posts/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
-const axios  = require("axios");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,16 +19,17 @@ app.post("/posts", async (req, res) => {
     id,
     title,
   };
-  await axios.post("http://localhost:4005/events", {
-    type: "PostCreated",
-    data: {
-      id,
-      title,
-    },
-  }).catch((err) => {
-    console.log(err.message);
-});
-;
+  await axios
+    .post("http://localhost:4005/events", {
+      type: "PostCreated",
+      data: {
+        id,
+        title,
+      },
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
   res.status(201).send(posts[id]);
 });
 
@@ -38,5 +39,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
+  console.log("v55");
   console.log("Listening on 4000");
 });
